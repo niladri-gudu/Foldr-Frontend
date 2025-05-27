5b6c683fe57b81ab499166655bf03e6fa3c2a5b1
Niladri-Gudu <niladrigudu@gmail.com>
2025-05-27 10:38:33 +0530

    finally fixed ig

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,23 @@
 import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'n-drive-nalco-mumbai.s3.ap-south-1.amazonaws.com',
-      }
-    ]
-  }
+      },
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://foldr-backend.vercel.app/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
